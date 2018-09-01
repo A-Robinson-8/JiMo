058e3f602b8f35ec1125a1dfac2c2a42743f2a6f
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2018-09-01 12:47:01 +0100

    Updated Player Movement

--- source/Player.hx
+++ source/Player.hx
@@ -9,14 +9,15 @@ import flixel.system.FlxAssets.FlxGraphicAsset;
 
 class Player extends FlxSprite {
 
-  public var topSpeed:Float = 500;
-  public var topAccel:Float = 20;
+  public var topSpeed:Float = 750;
+  public var baseAccel:Float = 0.1;
   public var hSpeed:Float = 0;
   public var vSpeed:Float = 0;
-  public var limit:Float = 20;
-  public var friction:Float = 2000;
+  public var limit:Float = 10;
+  public var friction:Float = 0.95;
   public var position:FlxPoint = null;
   public var mousePosition:FlxPoint = null;
+  public var distance:Float = null;
 
   public function new(?X:Float=0, ?Y:Float=0) {
     super(X, Y);
@@ -33,16 +34,21 @@ class Player extends FlxSprite {
 
     position = updatePosition();
     mousePosition = FlxG.mouse.getWorldPosition();
+    distance = position.distanceTo(mousePosition);
 
-    if (position.distanceTo(mousePosition) > limit) {
-      if (position.x < mousePosition.x) hSpeed += topAccel;
-      else hSpeed -= topAccel;
-      if (position.y < mousePosition.y) vSpeed += topAccel;
-      else vSpeed -= topAccel;
-    } else {
-      hSpeed = 0;
-      vSpeed = 0;
-    } velocity.set(hSpeed, vSpeed);
+    if (distance > limit) {
+      if (position.x < mousePosition.x) hSpeed += baseAccel * distance;
+      else hSpeed -= baseAccel * distance;
+      if (position.y < mousePosition.y) vSpeed += baseAccel * distance;
+      else vSpeed -= baseAccel * distance;
+    }
+
+    hSpeed = hSpeed * friction;
+    vSpeed = vSpeed * friction;
+    if (hSpeed > topSpeed) hSpeed = topSpeed;
+    if (vSpeed > topSpeed) vSpeed = topSpeed;
+    velocity.set(hSpeed, vSpeed);
+
   }
 
   public function updatePosition():FlxPoint {
