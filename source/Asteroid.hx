47e4f7634717c6a1277ae7b8f9ab5a7428781360
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2018-09-01 16:29:50 +0100

    Removed Spawner, Added Asteroids
    Moved spawn function to PlayState to simplify execution

--- source/Asteroid.hx
+++ source/Asteroid.hx
@@ -4,26 +4,21 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.math.FlxPoint;
+import flixel.system.FlxAssets.FlxGraphicAsset;
 
-class Spawner extends FlxSprite {
+class Asteroid extends FlxSprite {
 
   var rnd:Int = 0;
   var _asteroid = null;
 
   public function new(?X:Float=0, ?Y:Float=0) {
     super(X, Y);
+    makeGraphic(16, 16, FlxColor.WHITE);
+    velocity.set(Std.random(1000), Std.random(1000));
   }
 
   override public function update(elapsed:Float):Void {
-    spawn();
     super.update(elapsed);
   }
 
-  public function spawn() {
-    rnd = Std.random(120);
-    if (rnd == 1) {
-      _asteroid = new Asteroid();
-      add(_asteroid);
-    }
-  }
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -12,6 +12,14 @@ class PlayState extends FlxState {
 	}
 
 	override public function update(elapsed:Float):Void {
+		spawn();
 		super.update(elapsed);
+	}
+
+	public function spawn():Void {
+		if (Std.random(120) == 1) {
+			var _asteroid = new Asteroid(0, 0);
+			add(_asteroid);
+		}
 	}
 }
