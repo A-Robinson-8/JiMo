e5d0920dfade1a752def4601b2a99919f0bca68e
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2018-09-01 17:36:45 +0100

    Added Basic Fuel Bar

--- source/Hud.hx
+++ source/Hud.hx
@@ -1,15 +1,25 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxSprite;
+import flixel.ui.FlxBar;
 import flixel.util.FlxColor;
-import flixel.math.FlxPoint;
-import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.group.FlxGroup.FlxTypedGroup;
+using flixel.util.FlxSpriteUtil;
 
-class Hud extends FlxSprite {
+class Hud extends FlxTypedGroup<FlxSprite> {
 
-  public function new(?X:Float=0, ?Y:Float=0) {
-    super(X, Y);
+  var fuelBar:FlxBar = null;
+
+  public function new() {
+    super();
+    fuelBar = new FlxBar(0, 0, LEFT_TO_RIGHT, 960, 10);
+		fuelBar.createFilledBar(FlxColor.TRANSPARENT, FlxColor.WHITE, false);
+		add(fuelBar);
+    fuelBar.value = 100;
+    fuelBar.active = true;
+    visible = true;
   }
 
   override public function update(elapsed:Float):Void {
