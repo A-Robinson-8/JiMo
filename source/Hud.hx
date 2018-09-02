d7332e1c3a5615f37973bd2895998a43c105f286
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2018-09-02 14:19:50 +0100

    Added more fuel bars
    Because four fuel bars are always better than one

--- source/Hud.hx
+++ source/Hud.hx
@@ -10,21 +10,39 @@ using flixel.util.FlxSpriteUtil;
 
 class Hud extends FlxTypedGroup<FlxSprite> {
 
-  var fuelBar:FlxBar = null;
+  var fuelBarTop:FlxBar = null;
+  var fuelBarBtm:FlxBar = null;
+  var fuelBarLft:FlxBar = null;
+  var fuelBarRgt:FlxBar = null;
   var p:Player = null;
 
   public function new(p:Player) {
     super();
     this.p = p;
-    fuelBar = new FlxBar(0, 0, LEFT_TO_RIGHT, FlxG.width, 10);
-		fuelBar.createFilledBar(FlxColor.TRANSPARENT, FlxColor.WHITE, false);
-		add(fuelBar);
-    fuelBar.active = true;
+    fuelBarTop = new FlxBar(0, 0, FlxBarFillDirection.LEFT_TO_RIGHT, FlxG.width, 10);
+		fuelBarTop.createFilledBar(FlxColor.TRANSPARENT, FlxColor.WHITE, false);
+		add(fuelBarTop);
+    fuelBarTop.active = true;
+    fuelBarBtm = new FlxBar(0, FlxG.height - 10, FlxBarFillDirection.RIGHT_TO_LEFT, FlxG.width, 10);
+		fuelBarBtm.createFilledBar(FlxColor.TRANSPARENT, FlxColor.WHITE, false);
+		add(fuelBarBtm);
+    fuelBarBtm.active = true;
+    fuelBarLft = new FlxBar(0, 0, FlxBarFillDirection.BOTTOM_TO_TOP, 10, FlxG.height);
+		fuelBarLft.createFilledBar(FlxColor.TRANSPARENT, FlxColor.WHITE, false);
+		add(fuelBarLft);
+    fuelBarLft.active = true;
+    fuelBarRgt = new FlxBar(FlxG.width - 10, 0, FlxBarFillDirection.TOP_TO_BOTTOM, 10, FlxG.height);
+		fuelBarRgt.createFilledBar(FlxColor.TRANSPARENT, FlxColor.WHITE, false);
+	  add(fuelBarRgt);
+    fuelBarRgt.active = true;
     visible = true;
   }
 
   override public function update(elapsed:Float):Void {
-    fuelBar.value = p.fuel;
+    fuelBarTop.value = p.fuel;
+    fuelBarBtm.value = p.fuel;
+    fuelBarLft.value = p.fuel;
+    fuelBarRgt.value = p.fuel;
     super.update(elapsed);
   }
 
